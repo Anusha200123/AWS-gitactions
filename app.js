2b56ed96dd6a45ce0f2246190675fa4872fde05b
Anusha23-design <anusha.a@arithaconsulting.com>
2025-09-10 12:57:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hello from GitHub Actions + AWS EC2!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
